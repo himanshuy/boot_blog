fc9a3420f9e9cabfdb29a61627cad21a845203e5
yc09him <yc09him@macys.com>
2014-12-24 23:01:58 -0500

    Added basic java classes and home page

---
+++ src/main/java/controller/BlogController.java
@@ -0,0 +1,28 @@
+package controller;
+
+import model.Post;
+import service.BlogRepository;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Controller
+public class BlogController {
+
+	@Autowired
+	private BlogRepository repository;
+
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public String homePage(Model model) {
+
+		List<Post> posts = repository.findAll(); 
+		model.addAttribute("posts", posts);
+
+		return home;
+	}
+}
---
+++ src/main/java/model/Post.java
@@ -0,0 +1,97 @@
+package model;
+
+import java.util.Date;
+
+import org.springframework.data.annotation.Id;
+
+public class Post {
+	
+	@Id
+	private String id;
+
+	private String title;
+	private String content;
+	private Date dateCreated;
+	private String[] tags;
+
+	@Override
+	public String toString() {
+
+		return String.format(
+			"Post[id=%s, title=%s, dateCreated=%s, tags=%s]",
+			id, title, dateCreated, tags);
+
+	}
+	
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the title
+	 */
+	public String getTitle() {
+		return title;
+	}
+
+	/**
+	 * @param title the title to set
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	/**
+	 * @return the content
+	 */
+	public String getContent() {
+		return content;
+	}
+
+	/**
+	 * @param content the content to set
+	 */
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	/**
+	 * @return the dateCreated
+	 */
+	public Date getDateCreated() {
+		return dateCreated;
+	}
+
+	/**
+	 * @param dateCreated the dateCreated to set
+	 */
+	public void setDateCreated(Date dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+
+	/**
+	 * @return the tags
+	 */
+	public String[] getTags() {
+		return tags;
+	}
+
+	/**
+	 * @param tags the tags to set
+	 */
+	public void setTags(String[] tags) {
+		this.tags = tags;
+	}
+
+
+}
