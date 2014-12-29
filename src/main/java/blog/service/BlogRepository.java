c782324be196ccab48a1fbc7e89f0375ac8ddf24
himanshuy <yc09him@macys.com>
2014-12-29 16:50:46 -0500

    Added a comment

--- src/main/java/blog/service/BlogRepository.java
+++ src/main/java/blog/service/BlogRepository.java
@@ -4,10 +4,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import blog.model.Post;
 
-/**
- * Mongo Template for Blog App
- */
-
 public interface BlogRepository extends MongoRepository<Post, String> {
 
 	public Post findByTitle(String title);
