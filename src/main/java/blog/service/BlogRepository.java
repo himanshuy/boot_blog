7c4ac90852b161bd9a7c042877e9573b5d9c0555
yc09him <yc09him@macys.com>
2014-12-29 16:48:17 -0500

    Added a comment

--- src/main/java/blog/service/BlogRepository.java
+++ src/main/java/blog/service/BlogRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import blog.model.Post;
 
+/**
+ * Mongo Template for Blog App
+ */
+
 public interface BlogRepository extends MongoRepository<Post, String> {
 
 	public Post findByTitle(String title);
